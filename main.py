9fb9cd3107509d6a80187b26550418953637abe7
jobi joseph <jobijoseph16@gmail.com>
2025-01-29 18:47:28 +0530

    datas are added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
-    QTableWidget
+    QTableWidget, QTableWidgetItem
 import sys
-
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,11 +26,19 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        self.table
+        #connect the db
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        for row_numbers, row_data in enumerate(result):
+            self.table.insertRow(row_numbers)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_numbers, column_number,QTableWidgetItem(str(data)))
+        connection.close()
 
 
 #run the application
 app = QApplication(sys.argv)
 app_calculator = MainWindow()
+app_calculator.load_data()
 app_calculator.show()
 sys.exit(app.exec())
